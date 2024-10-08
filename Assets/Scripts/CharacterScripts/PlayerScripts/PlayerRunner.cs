1226d02105374495be0fca21365391f8aa312b35
Kancherla, Rohan <rohan360k@gmail.com>
2024-10-07 22:00:18 -0500

    Update PlayerRunner.cs

--- Assets/Scripts/CharacterScripts/PlayerScripts/PlayerRunner.cs
+++ Assets/Scripts/CharacterScripts/PlayerScripts/PlayerRunner.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 namespace MaskedMischiefNamespace
 {
-  [RequireComponent(typeof(PlayerInput))]
   public class PlayerRunner : MonoBehaviour
   {
-    public PlayerInput Input { get; private set; }
+    [SerializeField] private float cameraDistance;
+    private Transform cameraTransform;
     private PlayerMovementStateMachine movementStateMachine;
+    public GameInput gameInput;
+
 
     private void Awake()
     {
-      Input = GetComponent<PlayerInput>();
-
-      movementStateMachine = new PlayerMovementStateMachine();
+      cameraTransform = Camera.main.transform;
+      movementStateMachine = new PlayerMovementStateMachine(this);
     }
 
     private void Start()
@@ -26,6 +27,9 @@ namespace MaskedMischiefNamespace
     {
       movementStateMachine.HandleInput();
       movementStateMachine.Update();
+      cameraTransform.position = transform.position + transform.rotation * new Vector3(0, 0.75f, -(cameraDistance + 1));
+      cameraTransform.rotation = Quaternion.LookRotation(transform.position - cameraTransform.position) * Quaternion.Euler(-5, 0, 0);
+      cameraTransform.position += new Vector3(0, 1, 0);
     }
 
     private void FixedUpdate()
